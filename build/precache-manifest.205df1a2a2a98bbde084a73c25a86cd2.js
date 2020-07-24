0ab8b87d9b110138f82b208fb650176354f4b3bd
shubhkhanna <khannashubh04@gmail.com>
2020-07-24 11:55:17 +0530

    added

---
+++ build/precache-manifest.205df1a2a2a98bbde084a73c25a86cd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc53d5d608f660fb5b8bc8af7316d118",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "1644a5e74b7cd5e3408d",
+    "url": "/tic-tac-toe/static/css/2.23a9c941.chunk.css"
+  },
+  {
+    "revision": "4648ce25454a22161d9e",
+    "url": "/tic-tac-toe/static/css/main.43bd82ba.chunk.css"
+  },
+  {
+    "revision": "1644a5e74b7cd5e3408d",
+    "url": "/tic-tac-toe/static/js/2.0ead58ab.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.0ead58ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4648ce25454a22161d9e",
+    "url": "/tic-tac-toe/static/js/main.4ae07c7d.chunk.js"
+  },
+  {
+    "revision": "61109fc60463736a7ada",
+    "url": "/tic-tac-toe/static/js/runtime-main.12138588.js"
+  }
+]);
